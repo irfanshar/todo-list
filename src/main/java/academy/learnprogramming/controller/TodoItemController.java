ab299d8678863c0a0d3b65529714567b37db5df9
irfan Sharif <irfansharif009@gmail.com>
2021-08-07 11:03:04 -0400

    mid video

--- src/main/java/academy/learnprogramming/controller/TodoItemController.java
+++ src/main/java/academy/learnprogramming/controller/TodoItemController.java
@@ -1,14 +1,22 @@
 package academy.learnprogramming.controller;
 
 import academy.learnprogramming.model.TodoData;
+import academy.learnprogramming.model.TodoItem;
 import academy.learnprogramming.service.TodoItemService;
+import academy.learnprogramming.util.AttributeNames;
 import academy.learnprogramming.util.Mappings;
 import academy.learnprogramming.util.ViewNames;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.time.LocalDate;
+
+@Slf4j
 @Controller
 public class TodoItemController {
 
@@ -33,6 +41,25 @@ public class TodoItemController {
     @GetMapping(Mappings.ITEMS)
     public String items() {
         return ViewNames.ITEMS_LIST;
+    }
+
+    @GetMapping(Mappings.ADD_ITEM)
+    public String addEditItem(Model model) {
+        TodoItem todoItem = new TodoItem("", "", LocalDate.now());
+        model.addAttribute(AttributeNames.TODO_ITEM);
+
+        return ViewNames.ADD_ITEM;
+    }
+
+    // this method will be called automatically after form is submitted
+    // to get the object from the form we have to add the ModelAttribute annotation to
+    // todoItem parameter and spring mvc will automatically bind the form data to todoItem
+    @PostMapping(Mappings.ADD_ITEM)
+    public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) TodoItem todoItem) {
+        log.info("todoItem from form ={}", todoItem);
+        todoItemService.addItem(todoItem);
+
+        return "redirect:/" + Mappings.ITEMS;
     }
 
 }
